0631bdfc127a483e69b7ac8a05cba80f90287bbb
wtklbm <wtklbm@gmail.com>
2025-08-28 14:33:41 +0800

    ✨ feat: 添加依赖安装脚本

---
+++ scripts/install.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# shellcheck disable=SC2155,SC2207
+
+basedir="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)/.."
+
+export NODE_PATH="$(npm root -g)"
+
+# 提取模块名并转换为Bash数组
+PLUGINS=($(node < <(sed 's/\.map(v => require\.resolve(v))//g' "$basedir/.prettierrc.js" && echo "console.log(module.exports.plugins.join(' '))")))
+
+echo "正在安装插件: ${PLUGINS[*]} ..."
+
+npm i -g prettier "${PLUGINS[@]}"
